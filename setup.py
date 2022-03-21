0de88c0d606ec9eb207c2fb4709e0f3f32be458d
reimerrigtering <reimer.rigtering@gmail.com>
2022-03-21 13:19:01 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PygameCustomLibrary',
+    version='1.0.0',
+    author='Reimer Rigtering',
+    author_email='reimer.rigtering@gmail.com',
+    packages=find_packages()
+)
