59a28d3b69808bbd08fa3bdfb198c96de6c3e1c1
reimerrigtering <reimer.rigtering@gmail.com>
2022-03-28 12:25:03 +0200

    basic window script in tests.py

--- PygameCustomLibrary/tests.py
+++ PygameCustomLibrary/tests.py
@@ -1,1 +1,30 @@
 import main
+import pygame
+
+WIDTH, HEIGHT = 600, 600
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('test')
+
+
+def updateWindow():
+    pygame.display.update()
+
+
+def main():
+    test = True
+
+    while test:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                test = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    test = False
+
+        updateWindow()
+
+
+if __name__ == "__main__":
+    main()
