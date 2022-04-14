df2528747cfe33f00a439b72138a4945a2edfcaf
reimerrigtering <reimer.rigtering@gmail.com>
2022-04-14 10:35:37 +0200

    different classes seperately imported

--- PygameCustomLibrary/__init__.py
+++ PygameCustomLibrary/__init__.py
@@ -1,13 +1,2 @@
-from PygameCustomLibrary.main import Vector
-from PygameCustomLibrary.main import Direction
-from PygameCustomLibrary.main import Tag
-from PygameCustomLibrary.main import Frame
-from PygameCustomLibrary.main import Sprite
-from PygameCustomLibrary.main import Animation
-from PygameCustomLibrary.main import Text
-from PygameCustomLibrary.main import Shape
-from PygameCustomLibrary.main import Button
-from PygameCustomLibrary.main import Bar
-from PygameCustomLibrary.main import Board
-from PygameCustomLibrary.main import Scene
-
+from PygameCustomLibrary.main import Vector, Direction, Tag, Frame, Sprite, Animation, \
+    Text, Shape, Button, Bar, Board, Scene
