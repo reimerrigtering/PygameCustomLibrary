55544ce36a2466646196bbd464c436baadce627b
reimerrigtering <reimer.rigtering@gmail.com>
2022-04-14 10:35:02 +0200

    split all import statements

--- PygameCustomLibrary/__init__.py
+++ PygameCustomLibrary/__init__.py
@@ -1,2 +1,13 @@
-from PygameCustomLibrary.main import *
-print('run __init__.py')
+from PygameCustomLibrary.main import Vector
+from PygameCustomLibrary.main import Direction
+from PygameCustomLibrary.main import Tag
+from PygameCustomLibrary.main import Frame
+from PygameCustomLibrary.main import Sprite
+from PygameCustomLibrary.main import Animation
+from PygameCustomLibrary.main import Text
+from PygameCustomLibrary.main import Shape
+from PygameCustomLibrary.main import Button
+from PygameCustomLibrary.main import Bar
+from PygameCustomLibrary.main import Board
+from PygameCustomLibrary.main import Scene
+
